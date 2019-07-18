7c30b255ce4e56e1dbb3d335da9b93bff4542237
anatoliy266 <stakan993@gmail.com>
2019-07-18 18:04:23 +0300

    2

--- JsonFormsFactory/FormsFactory.cs
+++ JsonFormsFactory/FormsFactory.cs
@@ -14,11 +14,16 @@ namespace JsonFormsFactory
     public class FormsFactory
     {
         //string json = "{[Label: {name: label1, text: HelloWorld from formsFactory, anchor: [top, left], }, RichTextBox: {width: 100, height: 50, anchors: [top, label1]}]}";
-        string json = "{Label:  {Name: \"label1\", Text: \"HelloWorld from formsFactory\"}, RichTextBox : {Width: 150, Height: 50, Name: \"RichTextBox1\" }}";
-
+        string json = "{1: [{Label:  {Name: \"label1\", Text: \"HelloWorld from formsFactory\"}}, {RichTextBox : {Width: 150, Height: 50, Name: \"RichTextBox1\" }}], " +
+            "2: {ComboBox: {Name: \"ComboBox1\", Width: 100, Height: 40, Items: [\"Engineer\", \"Worker\", \"Student\", \"None\"]}}}";
+        ComboBox box = new ComboBox();
+        
         public FormsFactory()
         {
             Debug.WriteLine("here: create FormsFactory");
+            SQLConnector db = new SQLConnector("localhost\\SQLEXPRESS", "sa", "123456");
+            ComboBox box = new ComboBox();
+            box.Items.Add("");
         }
 
         public Form Generate()
@@ -34,55 +39,159 @@ namespace JsonFormsFactory
                 Debug.WriteLine("here: parseJson");
                 MessageBox.Show("here", "info");
                 var result = JsonConvert.DeserializeObject(json);
-                
+
                 return result;
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message, "error");
                 return null;
             }
-            
+
         }
 
         private Form GenerateForm(object jObj)
         {
             Form gForm = new Form();
+            gForm.Width = 200;
+            gForm.Height = 400;
 
-            
+            Button save = new Button();
+            save.Width = 50;
+            save.Height = 20;
+            save.Location = new System.Drawing.Point(0, 0);
+            save.Left = 0;
+            save.Top = gForm.Height - save.Height;
+            //save.Anchor = (AnchorStyles.Top | AnchorStyles.Left);
+            save.Text = "Save";
+
+            save.Parent = gForm;
+            //gForm.Controls.Add(save);
+
+            Button delete = new Button();
+            delete.Width = 50;
+            delete.Height = 20;
+            delete.Location = new System.Drawing.Point(0, 0);
+            //delete.Anchor = (AnchorStyles.Bottom | AnchorStyles.Left);
+            delete.Left = gForm.Width - delete.Width;
+            delete.Top = gForm.Height - delete.Height;
+            delete.AutoSize = true;
+            delete.Text = "Delete";
+
+            delete.Parent = gForm;
+            //gForm.Controls.Add(delete);
+            object control = null;
+
+            int x = 0;
+            int y = 0;
 
             var root = (JObject)jObj.GetType().InvokeMember("Root", BindingFlags.GetProperty, null, jObj, null);
 
+
             foreach (var val in root.Properties())
             {
+                if (val.Value.Type == JTokenType.Array)
+                {
+                    var array = (JArray)val.Value;
+                    foreach (var element in array.Children<JObject>())
+                    {
+                        var item = CreateControl(element);
+                        var placedItem = SetControlPosition(item, x, y);
+                        control = placedItem;
+                        var dY = Convert.ChangeType(control.GetType().GetProperty("Height").GetValue(control), typeof(Int32));
+                        y += (int)dY;
+                    }
+                    y = 0;
+                }
+                else if (val.Value.Type == JTokenType.Object)
+                {
+                    var item = CreateControl((JObject)val.Value);
+                    var placedItem = SetControlPosition(item, x, y);
+                    control = placedItem;
+                }
+                else throw new Exception("invalid json");
+                var dX = Convert.ChangeType(control.GetType().GetProperty("Height").GetValue(control), typeof(Int32));
+                x += (int)dX;
+
+                dynamic formControl = control;
+                gForm.Controls.Add(formControl);
+
+            }
+            gForm.Update();
+            return gForm;
+        }
+
+
+        private object CreateControl(JObject jItem)
+        {
+            object control = null;
+            foreach (var val in jItem.Properties())
+            {
                 MessageBox.Show(val.Name + ":::" + val.Value, "INFO");
-                Type type = Type.GetType("System.Windows.Forms."+val.Name+", System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", true, true);
+                Type type = Type.GetType("System.Windows.Forms." + val.Name + ", System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", true, true);
                 MessageBox.Show(type.ToString(), "INFO");
-                //var item = Assembly.GetExecutingAssembly().CreateInstance("System.Windows.Forms." + val.Name + ", System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", true);
-                //MessageBox.Show(val.Value.Type.ToString(), "INFO"); MessageBox.Show(item.GetType().ToString(), "INFO");
                 var createInstance = typeof(Activator).GetMethods().FirstOrDefault(mi => mi.Name == "CreateInstance" && mi.GetParameters().Count() == 0);
                 var createInstanceGen = createInstance.MakeGenericMethod(type);
                 var item = createInstanceGen.Invoke(null, null);
-                MessageBox.Show(val.Value.Type.ToString(), "INFO"); MessageBox.Show(item.GetType().ToString(), "INFO");
 
-                dynamic control = null;
                 if (val.Value.Type == JTokenType.Object)
                 {
-                    var properties = (JObject)val.Value;
-                    foreach (var property in properties.Properties())
+                    control = AddProperties(item, (JObject)val.Value);
+                }
+                else
+                {
+                    throw new Exception("invalid json");
+                }
+            }
+            return control;
+        }
+
+        private object AddProperties(object target, JObject properties)
+        {
+            object item = target;
+            if (properties != null)
+            {
+
+                var positionProperty = item.GetType().GetProperty("Location");
+                positionProperty.SetValue(item, new System.Drawing.Point(0, 0));
+                foreach (var property in properties.Properties())
+                {
+                    MessageBox.Show(property.Name + ":::" + property.Value);
+                    if (property.Value.Type == JTokenType.Array)
                     {
-                        MessageBox.Show(property.Name + ":::" + property.Value, "INFO");
-                        Label lb = new Label();
+                        var propertyItem = item.GetType().GetProperty(property.Name);
+                        //List<string> items = new List<string>();
+                        foreach (var arrayElement in property.Value)
+                        {
+                            MessageBox.Show(arrayElement.ToString(), "INFO");
+                            var itemsMethod = propertyItem.GetMethod.Invoke(item, null);
+                            itemsMethod.GetType().InvokeMember("Add", BindingFlags.InvokeMethod, null, itemsMethod, new object[] { arrayElement });
+                            //items.Add(arrayElement.ToString());
+                        }
+                        //propertyItem.GetType().InvokeMember("Add", BindingFlags.InvokeMethod, null, propertyItem, new object[] { items });
+                        
+                    } else if(property.Value.Type == JTokenType.Object)
+                    {
+                        var boxitems = box.Items;
+                    } else
+                    {
                         var propertyItem = item.GetType().GetProperty(property.Name);
                         var propertyType = propertyItem.PropertyType;
                         propertyItem.SetValue(item, Convert.ChangeType(property.Value, propertyType));
-                        control = item;
                     }
+                    
                 }
-                gForm.Controls.Add(control);
-                
             }
+            return item;
+        }
 
-            return gForm;
+        private object SetControlPosition(object target, int x, int y)
+        {
+            var item = target;
+            item.GetType().GetProperty("Left").SetValue(item, x);
+            item.GetType().GetProperty("Top").SetValue(item, y);
+            return item;
         }
     }
+        
 }
--- JsonFormsFactory/SQLConnector.cs
+++ JsonFormsFactory/SQLConnector.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace JsonFormsFactory
 {
     class SQLConnector
     {
+        SqlConnection SQLConnection;
         public SQLConnector(string host, string user, string pwd)
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
@@ -17,27 +19,87 @@ namespace JsonFormsFactory
             builder.Password = pwd;
             builder.InitialCatalog = "master";
 
-            var SQLConnection = new SqlConnection(builder.ConnectionString);
+            SQLConnection = new SqlConnection(builder.ConnectionString);
             SQLConnection.Open();
+            /*string query = "CREATE DATABASE formFactoryDB;\r\n" +
+                "USE formFactoryDB;" +
+                "CREATE TABLE JsonStorage(" +
+                "ID INT NOT NULL PRIMARY KEY AUTOINCREMENT(1,1), " +
+                "Json NVARCHAR);" +
+                "CREATE TABLE formData(" +
+                "PropertyID INT NOT NULL PRIMARY KEY AUTOINCREMENT(1,1)," +
+                "PropertyName VARCHAR(50), " +
+                "PropertyValue VARCHAR(50));";*/
+            string query = "If(db_id(N'formFactoryDB') IS NULL)\r\n" +
+                "BEGIN\r\n" +
+                "CREATE DATABASE [formFactoryDB];\r\n" +
+                "END";
+
+            string query1 = "USE formFactoryDB;" +
+                "if not exists (select name from sys.tables where name = 'JsonStorage')" +
+                "BEGIN " +
+                "CREATE TABLE dbo.JsonStorage" +
+                "(" +
+                "ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, " +
+                "Json NVARCHAR(50)" +
+                "); " +
+                "end";
+            string query2 = "USE formFactoryDB;" +
+                "if not exists (select name from sys.tables where name = 'formData')" +
+                "CREATE TABLE formData(" +
+                "PropertyID INT NOT NULL PRIMARY KEY IDENTITY(1,1)," +
+                "PropertyName VARCHAR(50), " +
+                "PropertyValue VARCHAR(50))";
+            SqlCommand comand = new SqlCommand(query, SQLConnection);
+            int rowsAffected = comand.ExecuteNonQuery();
+            //MessageBox.Show(rowsAffected.ToString(), "INFO");
+            comand = new SqlCommand(query1, SQLConnection);
+            rowsAffected = comand.ExecuteNonQuery();
+            //MessageBox.Show(rowsAffected.ToString(), "INFO");
+            comand = new SqlCommand(query2, SQLConnection);
+            rowsAffected = comand.ExecuteNonQuery();
+            //MessageBox.Show(rowsAffected.ToString(), "INFO");
         }
 
-        public bool CreateTable(string name, string[] options)
+        public bool CreateTable(string name, string[] fields)
         {
-            return false;
+            string query = "if not exists (select name from sys.tables where name = @tableName) CREATE TABLE @tableName(@fields)";
+            string _fields = "";
+            foreach (var field in fields)
+            {
+                _fields += field;
+            }
+            SqlCommand command = new SqlCommand(query, SQLConnection);
+            command.Parameters.AddWithValue("@tableName", name);
+            command.Parameters.AddWithValue("@fields", _fields);
+            int rowsAffected = command.ExecuteNonQuery();
+            if (rowsAffected == -1)
+            {
+                return true;
+            } else return false;
+
         }
 
-        public bool Insert()
+        public bool Insert(string table, string[] values)
         {
+            string query = "insert into @tableName(@fields) values (@values)";
             return false;
         }
 
         public bool Update()
         {
+            string query = "update @tableName set (@expression)";
             return false;
         }
 
-        public bool Remove()
+        public bool Remove(string table, string where = "")
         {
+            string query = "delete from @tableName";
+            if (where != "")
+            {
+                query += where;
+            }
+
             return false;
         }
     }
